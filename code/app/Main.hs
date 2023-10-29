ee322777b6dc8b105bd802eacdef969623101491
Artem Murashko <art22m@yandex-team.ru>
2023-10-30 00:49:18 +0300

    update cabal config

--- code/app/Main.hs
+++ code/app/Main.hs
@@ -3,8 +3,12 @@ module Main where
 import qualified Data.TestLib
 import HaskellSay (haskellSay)
 import qualified Data.PackedMemoryArray as PMA
+import Data.IntMap (size)
 
 main :: IO ()
 main = do 
     Data.TestLib.helloWorld
     haskellSay "Hello, art22m!"
+
+    let pma = PMA.init 8
+    print (PMA.size pma)
