e273ea15efe02ad2ed812b34ea9fefb5e02b3fd3
Artem Murashko <art22m@yandex-team.ru>
2023-10-29 19:32:44 +0300

    update cabal config

--- code/app/Main.hs
+++ code/app/Main.hs
@@ -1,4 +1,6 @@
 module Main where
 
+import qualified Data.TestLib
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = Data.TestLib.helloWorld
---
+++ code/src/Data/TestLib.hs
@@ -0,0 +1,4 @@
+module Data.TestLib where 
+
+helloWorld :: IO()
+helloWorld = putStrLn "Hello World from Data.TestLib"
