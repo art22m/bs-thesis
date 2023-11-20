eab23d7828b87e0bbd36ccde93e2636e9eafa44f
Artem Murashko <art22m@yandex-team.ru>
2023-11-20 16:07:23 +0300

    add fromZIndex and relevance check

--- code/src/Data/PackedMemoryQuadtree.hs
+++ code/src/Data/PackedMemoryQuadtree.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 
 module Data.PackedMemoryQuadtree where
 
@@ -10,25 +11,37 @@ import Data.List
 import qualified Data.PackedMemoryArrayMap as PMAMap
 import GHC.TypeLits (Nat)
 
----- Indexing
+---- ZIndex
 
 data Coords (n :: Nat) = Coords Int Int
+  deriving (Show)
 
 newtype ZIndex (n :: Nat) = ZIndex Int
   deriving (Eq, Ord, Show)
 
-zindexOf :: Coords n ->  ZIndex n
-zindexOf (Coords x y) =
-  ZIndex $ interleaveBinary x y
-
-interleaveBinary :: Int -> Int -> Int
-interleaveBinary x y = foldl' (\acc i -> acc .|. (shiftL (bitAt x i) (2 * i) .|. shiftL (bitAt y i) (2 * i + 1))) 0 shifts
-  where
+toZIndex :: Coords n ->  ZIndex n
+toZIndex (Coords x y) = ZIndex val
+  where 
     shifts = [0..max (finiteBitSize x) (finiteBitSize y) - 1]
-    
-    bitAt :: Int -> Int -> Int 
-    bitAt x' i = shiftR x' i .&. 1
+    val = foldl' (\acc i -> acc .|. (shiftL (bitAt x i) (2 * i) .|. shiftL (bitAt y i) (2 * i + 1))) 0 shifts
 
+fromZIndex :: ZIndex n -> Coords n
+fromZIndex (ZIndex val) = Coords x y
+  where
+    shifts = [0..(finiteBitSize val `div` 2 - 1)]
+    x = foldl' (\acc i -> acc .|. shiftL (bitAt val (2 * i)) i) 0 shifts
+    y = foldl' (\acc i -> acc .|. shiftL (bitAt val (2 * i + 1)) i) 0 shifts
+    
+bitAt :: Int -> Int -> Int 
+bitAt x' i = shiftR x' i .&. 1
+
+isRelevant :: ZIndex n -> ZIndex n -> ZIndex n -> Bool
+isRelevant minZIndex maxZIndex testZIndex = 
+  testX >= minX && testX <= maxX && testY >= minY && testY <= maxY
+  where
+    Coords minX minY = fromZIndex minZIndex 
+    Coords maxX maxY = fromZIndex maxZIndex
+    Coords testX testY = fromZIndex testZIndex
 
 ---- Data Structure
 
@@ -44,16 +57,16 @@ empty = Quadtree { getPMAMap = PMAMap.empty }
 singleton :: Coords n -> v -> Quadtree v
 singleton c v = Quadtree { getPMAMap = PMAMap.singleton zid v }
     where 
-        ZIndex zid = zindexOf c
+        ZIndex zid = toZIndex c
 
 lookup :: Coords n -> Quadtree v -> Maybe v
 lookup c qt = PMAMap.lookup zid pm
     where 
         pm = getPMAMap qt
-        ZIndex zid = zindexOf c
+        ZIndex zid = toZIndex c
 
 insert :: Coords n -> v -> Quadtree v -> Quadtree v
 insert c v qt = Quadtree { getPMAMap = PMAMap.insertP zid v pm }
     where 
         pm = getPMAMap qt
-        ZIndex zid = zindexOf c
+        ZIndex zid = toZIndex c
