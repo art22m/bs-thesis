83b66372553523ebc461fb8c63a65d3ee42e90d7
Artem Murashko <art22m@yandex-team.ru>
2023-11-27 22:04:23 +0300

    add region splitting

--- code/src/Data/PackedMemoryQuadtree.hs
+++ code/src/Data/PackedMemoryQuadtree.hs
@@ -26,14 +26,14 @@ toZIndex (Coords x y) = ZIndex val
     val = foldl' (\acc i -> acc .|. (shiftL (bitAt x i) (2 * i) .|. shiftL (bitAt y i) (2 * i + 1))) 0 shifts
 
 fromZIndex :: ZIndex n -> Coords n
-fromZIndex (ZIndex val) = Coords x y
+fromZIndex (ZIndex val) = Coords x y 
   where
     shifts = [0..(finiteBitSize val `div` 2 - 1)]
     x = foldl' (\acc i -> acc .|. shiftL (bitAt val (2 * i)) i) 0 shifts
     y = foldl' (\acc i -> acc .|. shiftL (bitAt val (2 * i + 1)) i) 0 shifts
     
 bitAt :: Int -> Int -> Int 
-bitAt x' i = shiftR x' i .&. 1
+bitAt x i = shiftR x i .&. 1
 
 isRelevant :: ZIndex n -> ZIndex n -> ZIndex n -> Bool
 isRelevant minZIndex maxZIndex testZIndex = 
@@ -42,6 +42,43 @@ isRelevant minZIndex maxZIndex testZIndex =
     Coords minX minY = fromZIndex minZIndex 
     Coords maxX maxY = fromZIndex maxZIndex
     Coords testX testY = fromZIndex testZIndex
+
+isHorizontalSplit :: ZIndex n -> ZIndex n -> Bool
+isHorizontalSplit (ZIndex l) (ZIndex r) = even (go shifts)
+  where 
+    shifts = reverse [0..max (finiteBitSize l) (finiteBitSize r) - 1]
+
+    go :: [Int] -> Int 
+    go [] = 0
+    go (x:xs) 
+      | bitAt l x == bitAt r x = go xs
+      | otherwise = x 
+
+bounds :: Int -> Int -> (Int, Int)
+bounds l r  = (litMax, bigMin)
+  where 
+    shifts = reverse [0..max (finiteBitSize l) (finiteBitSize r) - 1]
+
+    litMax = l .&. r .&. (shiftedVal - 1) .|. (shiftedVal - 1)
+    bigMin = l .&. r .&. shiftedVal .|. shiftedVal
+    shiftedVal = shiftL 1 (lastUncommon shifts)
+
+    lastUncommon :: [Int] -> Int 
+    lastUncommon [] = 0
+    lastUncommon (x:xs) 
+      | bitAt l x == bitAt r x = lastUncommon xs
+      | otherwise = x 
+
+splitRegion :: ZIndex n -> ZIndex n -> (ZIndex n, ZIndex n)
+splitRegion l r 
+  | isHorizontal = (toZIndex (Coords litMax yr), toZIndex (Coords bigMin yl))
+  | otherwise = (toZIndex (Coords xr litMax), toZIndex (Coords xl bigMin))
+    where 
+      Coords xl yl = fromZIndex l
+      Coords xr yr = fromZIndex r
+
+      isHorizontal = isHorizontalSplit l r
+      (litMax, bigMin) = if isHorizontal then bounds xl xr else bounds yl yr
 
 ---- Data Structure
 
