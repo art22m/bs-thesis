52ed1f0f676e73ba5eff0e73220ced22481f4d43
Artem Murashko <art22m@yandex-team.ru>
2024-02-18 20:25:36 +0300

    copy seq

--- src/Data/PackedMemoryQuadtree.hs
+++ src/Data/PackedMemoryQuadtree.hs
@@ -65,6 +65,14 @@ isRelevant minZIndex maxZIndex testZIndex =
     Coords maxX maxY = fromZIndex maxZIndex
     Coords testX testY = fromZIndex testZIndex
 
+isRelevant' :: Int -> Int -> Int -> Bool
+isRelevant' minZ maxZ testZ =
+  (minX <= testX && testX <= maxX) && (minY <= testY && testY <= maxY)
+  where
+    Coords minX minY = fromZIndex' minZ
+    Coords maxX maxY = fromZIndex' maxZ
+    Coords testX testY = fromZIndex' testZ
+
 -- bounds calculates LitMax and BigMin values.
 -- LitMax’s value of a and b can be calculated as ‘all common most significant bits’ in a and b followed by a 0 and then 1’s,
 -- and the BigMin’s value of a and b would be ‘all common most significant bits’ in a and b followed by 1 and then 0’s.
@@ -172,7 +180,45 @@ rangeLookupSeq (Coords x1 y1) (Coords x2 y2) qt = rangeLookupSeq' zl zr zl zr qt
 
 rangeLookupSeq' :: ZIndex n -> ZIndex n -> ZIndex n -> ZIndex n -> Quadtree v -> [(Coords n, v)]
 rangeLookupSeq' (ZIndex zl') (ZIndex zr') (ZIndex zl) (ZIndex zr) qt =
-  []
+    rangePMA (Map.getPMA pmaMap)
+    ++ rangeDMap (Map.getMap pmaMap)
+    ++ rangeNS (Map.getNS pmaMap)
+  where
+    pmaMap = getPMAMap qt
+
+    rangePMA :: PMA Int v -> [(Coords n, v)]
+    rangePMA pma = map (\(c, v) -> (fromZIndex' c, v)) (Vector.toList filteredPMA)
+      where
+        pmaCells = Vector.catMaybes (PMA.cells pma)
+        filteredPMA = Vector.filter (\(zind, _) -> (zl <= zind && zind <= zr && isRelevant (ZIndex zl') (ZIndex zr') (ZIndex zind))) pmaCells
+
+    rangeDMap :: DMap.Map Int v -> [(Coords n, v)]
+    rangeDMap dmap = map (\(c, v) -> (fromZIndex' c, v)) (DMap.toList filteredMap)
+      where
+        (_, rmap) = DMap.split (zl - 1) dmap
+        (lmap, _) = DMap.split (zr + 1) rmap
+        filteredMap = DMap.filterWithKey (\k _ -> isRelevant' zl' zr' k) lmap
+
+    rangeNS :: Map.NS Int v -> [(Coords n, v)]
+    rangeNS Map.M0 = []
+    rangeNS (Map.M1 as) = rangeChunk as
+    rangeNS (Map.M2 as bs _ rest) = rangeChunk as ++ rangeChunk bs ++ rangeNS rest
+    rangeNS (Map.M3 as bs cs _ rest) = rangeChunk as ++ rangeChunk bs ++ rangeChunk cs ++ rangeNS rest
+
+    rangeChunk :: Map.Chunk Int v -> [(Coords n, v)]
+    rangeChunk ch = map (\(c, v) -> (fromZIndex' c, v)) (Vector.toList filteredVector)
+      where
+        filteredVector = Vector.filter (\(zind, _) -> zl <= zind && zind <= zr && isRelevant' zl' zr' zind) ch
+
+rangeLookup :: Coords n -> Coords n -> Quadtree v -> [(Coords n, v)]
+rangeLookup (Coords x1 y1) (Coords x2 y2) qt = rangeLookup' (toZIndex cl) (toZIndex cr) qt
+  where
+    cl = Coords (min x1 x2) (min y1 y2)
+    cr = Coords (max x1 x2) (max y1 y2)
+
+rangeLookup' :: ZIndex n -> ZIndex n -> Quadtree v -> [(Coords n, v)]
+rangeLookup' (ZIndex zl) (ZIndex zr) qt =
+    []
     ++ rangePMA (Map.getPMA pmaMap)
     ++ rangeDMap (Map.getMap pmaMap)
     ++ rangeNS (Map.getNS pmaMap)
@@ -190,7 +236,7 @@ rangeLookupSeq' (ZIndex zl') (ZIndex zr') (ZIndex zl) (ZIndex zr) qt =
       where
         (_, rmap) = DMap.split (zl - 1) dmap
         (lmap, _) = DMap.split (zr + 1) rmap
-        filteredMap = DMap.filterWithKey (\k _ -> isRelevant (ZIndex zl') (ZIndex zr') (ZIndex k)) lmap
+        filteredMap = DMap.filterWithKey (\k _ -> isRelevant (ZIndex zl) (ZIndex zr) (ZIndex k)) lmap
 
     rangeNS :: Map.NS Int v -> [(Coords n, v)]
     rangeNS Map.M0 = []
@@ -202,20 +248,6 @@ rangeLookupSeq' (ZIndex zl') (ZIndex zr') (ZIndex zl) (ZIndex zr) qt =
     rangeChunk ch = map (\(c, v) -> (fromZIndex' c, v)) (Vector.toList filteredVector)
       where
         filteredVector = Vector.filter (\(zind, _) -> (zl <= zind && zind <= zr && isRelevant (ZIndex zl') (ZIndex zr') (ZIndex zind))) ch
-
-rangeLookup :: Coords n -> Coords n -> Quadtree v -> [(Coords n, v)]
-rangeLookup (Coords x1 y1) (Coords x2 y2) qt = rangeLookup'' (toZIndex cl) (toZIndex cr) qt
-  where
-    cl = Coords (min x1 x2) (min y1 y2)
-    cr = Coords (max x1 x2) (max y1 y2)
-
-rangeLookup' :: ZIndex n -> ZIndex n -> Quadtree v -> [(Coords n, v)]
-rangeLookup' zl zr qt = go zl zr qt ranges []
-  where
-    ranges = calculateRanges zl zr
-    go :: ZIndex n -> ZIndex n -> Quadtree v -> [(ZIndex n, ZIndex n)] -> [(Coords n, v)] -> [(Coords n, v)]
-    go zl' zr' qt' (r : rs) tmp = rangeLookupSeq' zl' zr' (fst r) (snd r) qt' ++ go zl' zr' qt' rs tmp
-    go _ _ _ [] tmp = tmp
 
 rangeLookup'' :: ZIndex n -> ZIndex n -> Quadtree v -> [(Coords n, v)]
 rangeLookup'' (ZIndex zl) (ZIndex zr) qt = go qt zl zr zl 0 []
